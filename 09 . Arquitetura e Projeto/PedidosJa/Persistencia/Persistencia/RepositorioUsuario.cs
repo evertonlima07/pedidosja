264939f2a105fd5e6bfbf36214a9d5bd41ffea68
Daniel Lima Oliveira <daniel102510@hotmail.com>
2017-08-18 12:43:10 -0300

    bibliotecas de model, negocio e persistencia

---
+++ 09 . Arquitetura e Projeto/PedidosJa/Persistencia/Persistencia/RepositorioUsuario.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistencia.Persistencia
+{
+    class RepositorioUsuario
+    {
+        //public RepositorioUsuario Adicionar(RepositorioUsuario usuario)
+    }
+}
