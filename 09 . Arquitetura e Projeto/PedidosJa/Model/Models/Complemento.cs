734fb497c95ee1a128485442a99798e0ced0aa9c
unknown <everton.lima07@hotmail.com>
2017-08-21 12:11:13 -0300

    Criada as classes Produto, Complemento e ProdutoComplemento

---
+++ 09 . Arquitetura e Projeto/PedidosJa/Model/Models/Complemento.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Models
+{
+    class Complemento
+    {
+        private int id;
+        private string descricao;
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public string Descricao
+        {
+            get { return descricao; }
+            set { descricao = value; }
+        }
+
+    }
+}
