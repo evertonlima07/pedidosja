23b582df221a7f8e4a8d5450f538d7a49f6aa45c
Daniel Lima Oliveira <daniel102510@hotmail.com>
2017-09-03 23:24:31 -0300

    separado empresa de cliente

--- 09 . Arquitetura e Projeto/PedidosJa/PedidosJa/Controllers/HomeController.cs
+++ 09 . Arquitetura e Projeto/PedidosJa/PedidosJa/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using PedidosJa.Util;
 using Model.Models;
 
@@ -13,6 +9,11 @@ namespace PedidosJa.Controllers
         public ActionResult Index()
         {
             //LOGIN AQUI
+            //ESSE CÓDIGO SERÁ USADO NO LOGOFF
+            SessionHelper.Set(SessionKeys.USUARIO, null);
+            SessionHelper.Set(SessionKeys.EMPRESA, null);
+            SessionHelper.Set(SessionKeys.TIPO_USER, null);
+
             return View();
         }
 
